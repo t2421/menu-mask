8ce8aad7fd21b7390d8430a505faf6793acc29e1
t2421 <t2421gm@gmail.com>
2020-04-21 10:48:09 +0900

    3つの要素で使いまわしてマスクを実現するよう実装

--- src/compile/scripts/main.js
+++ src/compile/scripts/main.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     var sections = [];
     var menu = [];
     var menuContainer = $('.menu-rect-container');
+    var currentId = "";
     $('.section').each((el,index)=>{
         sections.push($(el));
     });
@@ -13,17 +14,38 @@ window.addEventListener('DOMContentLoaded', (event) => {
         menu.push($(el));
     });
     $(window).on('scroll',()=>{
+        updateMask();
+    })
+    updateMask();
+    function updateMask(){
         menu.map(el =>{
             
-            var id = el.data("id");
-            var targetSection = $("#"+id);
+            var id = el.attr("data-id");
+            var targetSection = $("#section-"+id);
+            if(targetSection.length == 0){
+                $(el).height(0);
+                return;
+            }
             var navTop = $(el).offset().top;
             var navBottom = navTop + menuContainer.height();
             var sectionTop = targetSection.offset().top;
             var sectionBottom = sectionTop+targetSection.innerHeight();
             var diff = navBottom-sectionBottom;
             var height = Math.min(menuContainer.height()-diff,menuContainer.height())
+            if(navTop >= sectionTop && navBottom <= sectionBottom){
+                if(id != currentId){
+                    currentId = id;
+                    updateMaskItem();
+                }
+            }
             $(el).height(height);
         });
-    })
+    }
+    function updateMaskItem(){
+        $('.is-current').removeClass('is-current')
+        $("#"+currentId).addClass("is-current");
+        $("#menu-rect--prev").attr('data-id',Number(currentId)-1)
+        $("#menu-rect--current").attr('data-id',Number(currentId))
+        $("#menu-rect--next").attr('data-id',Number(currentId)+1)
+    }
 });
